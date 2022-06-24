2560865cf90fef8e6294875d1929a58b4f4c5561
Daniel P. Shannon <daniel@try.works>
2022-06-24 05:06:23 -0400

    Add Hyphenopoly.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,22 +7,65 @@ import '@spectrum-css/vars/dist/spectrum-large.css'
 import emotionTailwindPreflight from 'emotion-tailwind-preflight'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import Script from 'next/script'
 
 import { AdobeClean } from 'assets/adobe-clean'
 import { Body } from 'styles/global'
 import { theme } from 'styles/theme'
 
+declare global {
+  interface Window {
+    Hyphenopoly: {
+      config: (...args: unknown[]) => void
+    }
+  }
+}
+
 function SandboxApp ({ Component, pageProps }: AppProps) {
   return (
     <>
       <Global styles={ emotionTailwindPreflight } />
+
       <AdobeClean />
+
+      <Script
+        src='/hyphenopoly/Hyphenopoly_Loader.js'
+        strategy='lazyOnload'
+        onLoad={ () => {
+          window.Hyphenopoly.config({
+            paths: {
+              maindir: '/hyphenopoly/',
+              patterndir: '/hyphenopoly/',
+            },
+            require: {
+              'en-us': 'FORCEHYPHENOPOLY',
+            },
+            setup: {
+              defaultLanguage: 'en-us',
+              hide: 'false',
+              keepAlive: true,
+              normalize: false,
+              selectors: {
+                body: {
+                  compound: 'all',
+                  hyphen: '\u00AD',
+                  orphanControl: 3,
+                },
+              },
+              timeout: 1000,
+            },
+          })
+        } } />
+
       <Head>
-        <html className='spectrum spectrum--large spectrum--dark spectrum-Body spectrum-Body--sizeM' />
+        <html
+          className='spectrum spectrum--large spectrum--dark spectrum-Body spectrum-Body--sizeM'
+          lang='en-us' />
         <title>Sandbox</title>
         <meta content='initial-scale=1.0, width=device-width' name='viewport' />
         <meta content='IE=edge' httpEquiv='X-UA-Compatible' />
       </Head>
+
       <ThemeProvider theme={ theme }>
         <Body />
         <Component { ...pageProps } />
