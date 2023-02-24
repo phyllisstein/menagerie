24a664f31cbaf1339a6ff7c5aa3d612ee8e1fe30
Daniel P. Shannon <daniel@try.works>
2023-02-23 22:20:35 -0500

    💫 Second peephole.

--- src/pages/hatch.tsx
+++ src/pages/hatch.tsx
@@ -10,11 +10,6 @@ const AnimatedFeColorMatrix = animated('feColorMatrix')
 
 const Container = styled.div`
   position: absolute;
-  top: 50%;
-  left: 50%;
-
-  width: 50vw;
-  height: 50vh;
 
   transform: translate3d(-50%, -50%, 0);
   transform-style: preserve-3d;
@@ -28,8 +23,11 @@ const MiddleVideo = styled.video`
   top: 50%;
   left: 50%;
 
+  width: 100vw;
+  height: 100vh;
+
   transform: translate3d(-50%, -50%, -5rem);
-  filter: url(#red-wash);
+  filter: url(#animated-wash-2);
 `
 
 const FrontVideo = styled.video`
@@ -44,7 +42,7 @@ const FrontVideo = styled.video`
   filter: url(#animated-wash);
 `
 
-const Peephole = styled.div`
+const MidPeephole = styled.div`
   position: absolute;
   top: 0;
   left: 0;
@@ -56,6 +54,23 @@ const Peephole = styled.div`
   perspective: 1000px;
 
   mask-image: url('/assets/hatch/mkpm.png');
+  mask-position: 50% 50%;
+  mask-repeat: no-repeat;
+  mask-size: 50% 50%;
+`
+
+const FrontPeephole = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  width: 100vw;
+  height: 100vh;
+
+  transform-style: preserve-3d;
+  perspective: 1000px;
+
+  mask-image: url('/assets/hatch/cdm.png');
   mask-position: 50% 50%;
   mask-repeat: no-repeat;
   mask-size: 50% 50%;
@@ -79,16 +94,17 @@ function HatchPage () {
   `
 
   const redMatrix = `
-    1   1     0.5     0   0
+    0.5   0     0     0   0
     0     0   0       0 0
     0     0     0   0   0
-    0     0     0     1   0
+    1     0     0     1   0
   `
 
-  const [{ values }, api] = useSpring(() => ({
+  const [values, api] = useSpring(() => ({
     config: config.molasses,
     from: {
-      values: identityMatrix,
+      washOne: identityMatrix,
+      washTwo: identityMatrix,
     },
   }))
 
@@ -106,25 +122,35 @@ function HatchPage () {
 
     if (first) {
       api.start({
-        values: frontPurple ? purpleMatrix : redMatrix,
+        washOne: frontPurple ? purpleMatrix : redMatrix,
+        washTwo: frontPurple ? redMatrix : purpleMatrix,
       })
     }
 
     if (active) {
-      gsap.to('#peephole', {
-        maskPosition: `${ offsetX }% ${ offsetY }%`,
-        maskSize: `${ 100 - offsetX }% ${ 100 - offsetY }%`,
+      gsap.to('#front-peephole', {
+        rotateX: `${ offsetY / 2 }deg`,
+        rotateY: `${ offsetX / 2 }deg`,
+      })
+      gsap.to('#mid-peephole', {
+        perspective: (x + window.innerWidth) * 2,
+        z: `${ -offsetY / 2 }px`,
       })
     }
 
     if (last) {
-      gsap.to('#peephole', {
-        maskPosition: '50% 50%',
-        maskSize: '50% 50%',
+      gsap.to('#front-peephole', {
+        rotateX: 0,
+        rotateY: 0,
+      })
+      gsap.to('#mid-peephole', {
+        perspective: 1000,
+        z: 0,
       })
 
       api.start({
-        values: identityMatrix,
+        washOne: identityMatrix,
+        washTwo: identityMatrix,
       })
 
       setFrontPurple(!frontPurple)
@@ -140,7 +166,7 @@ function HatchPage () {
   )
 
   return (
-    <div>
+    <Container>
       <svg viewBox='0 0 10 10' width='0' height='0'>
         <defs>
           <filter id='purple-wash'>
@@ -154,16 +180,24 @@ function HatchPage () {
       <animated.svg viewBox='0 0 10 10' width='0' height='0'>
         <defs>
           <filter id='animated-wash'>
-            <AnimatedFeColorMatrix type='matrix' values={ values } />
+            <AnimatedFeColorMatrix type='matrix' values={ values.washOne } />
+          </filter>
+          <filter id='animated-wash-2'>
+            <AnimatedFeColorMatrix type='matrix' values={ values.washTwo } />
           </filter>
         </defs>
       </animated.svg>
-      <Peephole id='peephole'>
+      <MidPeephole id='mid-peephole'>
+        <MiddleVideo autoPlay loop muted playsInline>
+          <source src='/assets/hatch/coverr-jeronimos-monastery-in-lisbon-portugal-6360-original.mp4' type='video/mp4' />
+        </MiddleVideo>
+      </MidPeephole>
+      <FrontPeephole id='front-peephole'>
         <FrontVideo autoPlay loop muted playsInline>
           <source src='/assets/hatch/coverr-a-vinyl-disc-rotating-on-a-record-player-6767-original.mp4' type='video/mp4' />
         </FrontVideo>
-      </Peephole>
-    </div>
+      </FrontPeephole>
+    </Container>
   )
 }
 
