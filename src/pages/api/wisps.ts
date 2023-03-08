df258306ae83323ce17e31d432ad4760713e8aca
Daniel P. Shannon <daniel@try.works>
2023-03-07 21:53:23 -0500

    💩 State not quite right

--- src/pages/api/wisps.ts
+++ src/pages/api/wisps.ts
@@ -2,30 +2,40 @@ import { type NextApiHandler } from 'next'
 import { type NextWebSocketHandler } from 'next-plugin-websocket'
 import { type WebSocket } from 'ws'
 
-const sockets = new Set<WebSocket>()
+interface Wisp {
+  id: string
+  position: [number, number, number]
+}
+
+const state = new Map<WebSocket, Wisp>()
 
 export const socket: NextWebSocketHandler = (ws, req) => {
-  if (!sockets.has(ws)) {
-    sockets.add(ws)
-
-    ws.on('close', () => {
-      sockets.delete(ws)
+  if (!state.has(ws)) {
+    ws.addEventListener('close', () => {
+      state.delete(ws)
     })
 
-    ws.on('message', message => {
-      console.log(message.toString())
+    ws.addEventListener('message', message => {
+      const { data } = message
 
-      ws.send(JSON.stringify(
-        {
-          position: [
-            25,
-            250,
-            25,
-          ],
-        },
-      ))
+      if (data.type === 'register') {
+        const position = [
+          Math.floor(Math.random() * 25),
+          Math.floor(Math.random() * 250),
+          Math.floor(Math.random() * 250),
+        ]
+        const wisp = { id: data.id, position }
+        console.log(wisp)
+        state.set(ws, wisp)
+        const states = Array.from(state.values())
+        ws.send(JSON.stringify({ type: 'update', data: states }))
+      }
     })
   }
+
+  const states = Array.from(state.values())
+  console.log(states)
+  ws.send(JSON.stringify({ type: 'update', data: states }))
 }
 
 const handler: NextApiHandler = async (req, res) => {
